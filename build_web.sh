291c208c720dd8055b477ae0a6649fa7672bf70d
0/0 <zero@imaskeleton.me>
2021-09-08 22:53:57 -0600

    update build script

--- build_web.sh
+++ build_web.sh
@@ -4,7 +4,7 @@ set -eu
 # ./setup_web.sh # <- call this first!
 
 FOLDER_NAME=${PWD##*/}
-CRATE_NAME=$FOLDER_NAME # assume crate name is the same as the folder name
+CRATE_NAME="ferrischat_client" # assume crate name is the same as the folder name
 CRATE_NAME_SNAKE_CASE="${CRATE_NAME//-/_}" # for those who name crates with-kebab-case
 
 # This is required to enable the web_sys clipboard API which egui_web uses
